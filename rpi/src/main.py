cba8cf0e55a4b590f479d22d6b39064bf897d4d9
J3T21 <jameset701@gmail.com>
2024-01-29 20:34:49 +0000

    started with gas sensor not working properly yet identifying libraries for tof

--- rpi/src/main.py
+++ rpi/src/main.py
@@ -2,7 +2,7 @@ import time
 import smbus2
 import requests as r
 
-class MySensor:
+class Temp_Humid_Sensor:
     def __init__(self, address=0x40): # default temperature master hold
         self.add = address
         self.tempCommand = 0xe3 # Temperature, Master No Hold
@@ -30,15 +30,40 @@ class MySensor:
     def _updateSensor(self, command):
         cmd_meas = smbus2.i2c_msg.write(self.add, [command])
         self.bus.i2c_rdwr(cmd_meas)
-           
+
+class Gas_Sensor:
+    def __init__(self, address=0x5A):
+        self.add = address
+        self.status = 0x00
+        self.mode = 0x01
+        self.result = 0x02
+        self.bus = smbus2.SMBus(1)
+
+    def read_ccs811_data(self):
+    # Check if data is ready
+        ready = self.bus.read_byte_data(self.add, self.status)
+        if ready & (1 << 3):  # Bit 3 indicates data ready
+            # Read the data
+            data = self.bus.read_i2c_block_data(self.add, self.result, 8)  
+            # Process the data
+            co2 = (data[0] << 8) | data[1]
+            tvoc = (data[2] << 8) | data[3]
+            return co2, tvoc
+        else:
+            return None
+
+
             
 def main():
     url = ""
     samplingTime = 0.1
-    sensor = MySensor()
+    T_H_sensor = Temp_Humid_Sensor()
+    Gas_Sensor = Gas_Sensor()
     while True:
-        temp, rh = sensor.read()
+        temp, rh = T_H_sensor.read()
         print(f'temp:{temp}, humidity:{rh}')
+        co2, tvoc = Gas_Sensor.read_ccs811_data()
+        print(f'co2:{co2}, tvoc:{tvoc}')
         # backPressure = r.post(url=url, data={"temp":temp, "humid":rh}) 
         
         #TODO: If needed, implement back pressure, i.e make sample time inversly proportional to value obtained from server   
