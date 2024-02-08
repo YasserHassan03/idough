36bf6f5eb10fe973c126154fd7fd15ef5f7db0c1
YHassan3 <yh@nexcomglobal.com>
2024-02-08 14:34:52 +0000

    flask server running

--- AWS-Stuff/app.py
+++ AWS-Stuff/app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask,request, jsonify 
 
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/pause')
 def hello():
         return 'Hello World!'
 
+@app.route('/', methods=['POST','GET'])
+def json():
+        #title = request.json()
+        #content = request.json['content']
+        data = request.json
+        print(data['temp'])
+        data= 1
+        return jsonify(data)
+
 if __name__ == '__main__':
-        app.run()
+        app.run(host='0.0.0.0', port=8080, debug=True)
 
 
